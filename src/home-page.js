31f2ca696f827d449f83b255fcb75b43b70d7ba5
Twofold-One <evan_geras@mail.ru>
2021-10-09 00:34:12 +0700

    home-page js ready and implemented

--- src/home-page.js
+++ src/home-page.js
@@ -1,8 +1,98 @@
-// Function which generates home
+// Function which generates home page
 function createHome() {
     const home = document.createElement('div');
+    home.id = 'home';
+    home.className = 'home';
+    appendAllDiv();
+
+    function createHomeHeading() {
+        const homeHeading = document.createElement('div');
+        homeHeading.id = 'heading';
+        homeHeading.className = 'content-element';
+
+        const homeHeadingH1 = document.createElement('h1');
+        homeHeadingH1.textContent = 'Random PUB';
+
+        const homeHeadingH2 = document.createElement('h2');
+        homeHeadingH2.textContent = 'For random folks';
+
+        const homeHeadingImg = document.createElement('img');
+        homeHeadingImg.src = '/src/PUB.jpeg';
+        homeHeadingImg.alt = 'PUB image';
+
+        homeHeading.appendChild(homeHeadingH1);
+        homeHeading.appendChild(homeHeadingH2);
+        homeHeading.appendChild(homeHeadingImg);
+        
+        return homeHeading;
+    }
+
+    function createHomeReview() {
+        const homeReview = document.createElement('div');
+        homeReview.id = 'review';
+        homeReview.className = 'content-element';
+
+        const homeReviewH2 = document.createElement('h2');
+        homeReviewH2.textContent = 'Welcome';
+        
+        const homeReviewPara = document.createElement('p');
+        homeReviewPara.textContent = 'to the "Random PUB for random folks", where you could eat & drink some random stuff, also deal with some random activity';
+
+        homeReview.appendChild(homeReviewH2);
+        homeReview.appendChild(homeReviewPara);
+
+        return homeReview;
+    }
+
+    function createHomeWorkingTime() {
+        const homeWorkingTime = document.createElement('div');
+        homeWorkingTime.id = 'working-time';
+        homeWorkingTime.className = 'content-element';
+
+        const homeWorkingTimeH2 = document.createElement('h2');
+        homeWorkingTimeH2.textContent = 'Bizarre working hours';
+        homeWorkingTime.appendChild(homeWorkingTimeH2);
+        
+        const homeWorkingTimeList = document.createElement('ul');
+        homeWorkingTimeList.className ='working-time-list';
+
+        createListElement('Monday: random time between 8am - 8pm');
+        createListElement('Tuesday: random time between 8am - 8pm');
+        createListElement('Wednesday: random time between 8am - 8pm');
+        createListElement('Thursday: random time between 8am - 8pm');
+        createListElement('Friday: random time between 8am - 8pm');
+        createListElement('Saturday: random time between 6am - 10pm');
+        createListElement('Sunday: random time between 6am - 10pm');
+
+        function createListElement(text) {
+            const listElement = document.createElement('li');
+            listElement.textContent = text;
+            homeWorkingTimeList.appendChild(listElement);
+
+            return listElement;
+        }
+
+        return homeWorkingTime;
+
+    }
+
+    function appendAllDiv() {
+        home.appendChild(createHomeHeading());
+        home.appendChild(createHomeReview());
+        home.appendChild(createHomeWorkingTime());
+    }
+
     return home;
 }
 
+// function addHomeToContent() {
+//     const content = document.getElementById('content');
+//     content.textContent = '';
+//     content.appendChild(createHome());
+// }
+
+export default createHome;
+
+
 // const checker = () => console.log('home-page connected');
-// export default checker;
+// export {checker};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import _ from 'lodash';
 import './style.css';
-import addDOMElement from './home-page';
+import createHome from './home-page';
+
+const content = document.getElementById('content');
+content.appendChild(createHome());
+
+const homeBtn = document.getElementById('home');
+homeBtn.addEventListener('click', () => {
+    homeBtn.classList.add('active');
+});
+
 
 // function checkConnection() {
 //     console.log('check three times, ok!');
