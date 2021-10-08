17f13e4e7fb65d09da473ae5081a1d883af9c67f
Twofold-One <evan_geras@mail.ru>
2021-10-08 15:49:16 +0700

    overall styling and home page content

---
+++ src/home-page.js
@@ -0,0 +1,8 @@
+// Function which generates home
+function createHome() {
+    const home = document.createElement('div');
+    return home;
+}
+
+// const checker = () => console.log('home-page connected');
+// export default checker;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import './style.css';
+import addDOMElement from './home-page';
 
-function checkConnection() {
-    console.log('check three times, ok!');
-}
-
-checkConnection();
+// function checkConnection() {
+//     console.log('check three times, ok!');
+// }
+// checkConnection();
